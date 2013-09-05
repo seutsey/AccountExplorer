4731bdd1aba3fd32c7ab54b14cddbd66151e5950
seutsey <seutsey@gmail.com>
2013-09-05 14:01:54 -0400

    Added XML data store for username and passwords

--- AccountExplorer/Program.cs
+++ AccountExplorer/Program.cs
@@ -8,6 +8,7 @@ using OpenQA.Selenium.Firefox;
 using OpenQA.Selenium.Support.UI;
 using OpenQA.Selenium.Chrome;
 using System.Security.Cryptography;
+using System.Xml.Linq;
 
 namespace AccountExplorer
 {
@@ -16,36 +17,46 @@ namespace AccountExplorer
         static void Main(string[] args)
         {
             IWebDriver driver = new FirefoxDriver();
+            XDocument xdoc = XDocument.Load("WebsiteData.xml");
 
-            //SHA1 sha = new SHA1CryptoServiceProvider();
-            //MD5 md5 = new MD5CryptoServiceProvider();
+            IEnumerable<XElement> websiteInfo = from w in xdoc.Descendants("website")
+                                                select w;
 
-            //string hash = "a9xstCndTiLqqEiGgpcv1g==";
+            foreach (XElement x in websiteInfo)
+            {
+                string _website = x.Attribute("id").Value.ToString();
+                driver.Navigate().GoToUrl(_website);
 
-            //ToBase64Transform b64 = new ToBase64Transform();
-            //byte[] pword = System.Convert.FromBase64String(hash);
-            //string AttPassword = System.Text.Encoding.UTF8.GetString(pword);
+                switch (_website)
+                {
+                    case "http://att.com":
+                        Att_Type(x, driver);
+                        break;
+                    default:
+                        break;
+                }
 
-            driver.Navigate().GoToUrl("https://www.att.com");
-
-            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
-            wait.Until((d) => { return driver.FindElement(By.Id("userid")); });
-
-            IWebElement phonenumber = driver.FindElement(By.Id("userid"));
-            phonenumber.SendKeys("724-708-2449");
-            IWebElement password = driver.FindElement(By.Id("userPassword"));
-            
-            password.Submit();
-
-            //wait.Until((d) => { return d.Title.ToLower().StartsWith("Account"); });
-            System.Console.WriteLine("page title is: " + driver.Title);
-            wait.Until((d) => { return d.Title.ToLower().StartsWith("account"); });
-
-            System.Console.WriteLine("ATT Balance: " + driver.FindElement(By.ClassName("font30imp")).Text);
-
+            }
             driver.Quit();
 
             Console.ReadKey();
+        }
+
+        static void Att_Type(XElement x, IWebDriver driver)
+        {
+            string _username = x.Element("username").Value.ToString();
+            string _password = x.Element("password").Value.ToString();
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+            wait.Until((d) => { return driver.FindElement(By.Id("userid")); });
+                
+            IWebElement phonenumber = driver.FindElement(By.Id("userid"));
+            phonenumber.SendKeys(_username);
+            IWebElement password = driver.FindElement(By.Id("userPassword"));
+            password.SendKeys(_password);
+
+            wait.Until((d) => { return d.Title.ToLower().StartsWith("account"); });
+
+            System.Console.WriteLine("ATT Balance: " + driver.FindElement(By.ClassName("font30imp")).Text);
         }
     }
 }
